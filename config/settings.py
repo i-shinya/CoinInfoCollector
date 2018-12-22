3a0a1fdb21bb51172ebe46a922b35a1d1d837203
i-shinya <sora0376@gmail.com>
2018-12-23 05:33:57 +0900

    修正

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # 環境変数でDJANGO_HEROKU_FLAGをTrueにしておくと環境変数を読んでくれる。
 # Falseの場合はローカル設定ファイルを読み込む
-READ_ENV_FILE = os.environ.get("DJANGO_HEROKU_FLAG", default=True)
+READ_ENV_FILE = os.environ.get("DJANGO_HEROKU_FLAG", default=False)
 if READ_ENV_FILE:
     DATABASES = {
         "default": {
