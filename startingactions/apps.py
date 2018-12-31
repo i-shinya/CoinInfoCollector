defe4c01b61e23060db3e8b5cf0308ab738e5aae
i-shinya <sora0376@gmail.com>
2019-01-01 03:51:15 +0900

    add startingaction app

---
+++ startingactions/apps.py
@@ -0,0 +1,26 @@
+# 以下でbatchスケジューラーでの定義を行うスケジュールの定義
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.apps import AppConfig
+import os
+
+
+class StartingActions(AppConfig):
+    name = "startingactions"
+    verbose_name = "Starting Application"
+
+    def ready(self):
+        sched = BackgroundScheduler()
+        # 環境変数でスケジュールフラグがTrueの場合のみスケジュールを設定する。
+        # 文字列で返却されるため文字列で判定
+        print("[DEBUG]Scheduler read.")
+        sched.start()
+
+        @sched.scheduled_job("interval", minutes=1)
+        def shedule():
+            print("Scheduled job start.")
+            from batch import apis
+
+            service = apis.BatchScheduleServise()
+            service.scheduleAction()
+            print("Scheduled job finish.")
+
