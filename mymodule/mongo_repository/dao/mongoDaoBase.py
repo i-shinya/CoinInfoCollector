a8cb7e1ef4caa0d5eae954257275a115f1ab4ca7
i-shinya <sora0376@gmail.com>
2019-02-11 22:06:45 +0900

    add auth settings

--- mymodule/mongo_repository/dao/mongoDaoBase.py
+++ mymodule/mongo_repository/dao/mongoDaoBase.py
@@ -16,10 +16,10 @@ class MongoDaoBase:
         cls.client = MongoClient(
             MONGODB_INFOS["HOST"],
             MONGODB_INFOS["PORT"],
-            # username=MONGODB_INFOS["USER"],
-            # password=MONGODB_INFOS["PASSWORD"],
-            # authSource=MONGODB_INFOS["NAME"],
-            # authMechanism="SCRAM-SHA-256",
+            username=MONGODB_INFOS["USER"],
+            password=MONGODB_INFOS["PASSWORD"],
+            authSource=MONGODB_INFOS["NAME"],
+            authMechanism="SCRAM-SHA-256",
         )
         cls.db = cls.client[MONGODB_INFOS["NAME"]]
         cls.collection = cls.db["trade_data"]  # コレクションは使用するものが追加されたらべた書きする
